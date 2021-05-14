c6127ea65d79add11eb7ee93edaced3fc22ea25a
SachinWankhade67369 <sonofrj19@gmail.com>
2021-05-14 14:42:35 +0530

    pushing challenges

---
+++ html/rotate.js
@@ -0,0 +1,16 @@
+
+let nums=[1,2,3,6,9,8,7,4];
+let id=[1,2,3,6,9,8,7,4];
+
+function rotate(){
+    
+   // document.getElementById('btn1').innerHTML=bt[6];
+    for( i=0;i<bt.length;i++)
+    {
+        const data=bt[i];
+        console.log(data);
+        //document.getElementById('btn1').innerHTML=data;
+    }
+  
+}
+rotate();
---
+++ javascript challenges/foo-bar.js
@@ -0,0 +1,21 @@
+
+for(i=1;i<=30;i++)
+{
+    //let o="";
+    if(i%15==0)
+    {
+        console.log("foo-bar")
+    }
+    else if(i%3===0)
+    {
+        console.log("foo");
+        
+    }
+    else if(i%5===0){
+       console.log("bar");
+    }
+    else{
+        console.log(i);
+    }
+    
+}
